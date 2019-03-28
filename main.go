4904a6d4bfdd807d8752c2bd5db8c96d65691059
yusuke ohno <yusuke@ohno.systems>
2019-03-28 22:06:12 +0900

    Make the Faker text output rainbow

--- main.go
+++ main.go
@@ -3,8 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/enodata/faker"
+	"math"
 	"strings"
 )
+
+func rgb(i int) (int, int, int) {
+	var f = 0.1
+	return int(math.Sin(f*float64(i)+0)*127 + 128),
+		int(math.Sin(f*float64(i)+2*math.Pi/3)*127 + 128),
+		int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
+}
 
 func main() {
 	var phrases []string
@@ -13,5 +21,11 @@ func main() {
 		phrases = append(phrases, faker.Hacker().Phrases()...)
 	}
 
-	fmt.Println(strings.Join(phrases[:], "; "))
+	output := strings.Join(phrases[:], "; ")
+
+	for j := 0; j < len(output); j++ {
+		r, g, b := rgb(j)
+		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
+	}
+	fmt.Println()
 }
